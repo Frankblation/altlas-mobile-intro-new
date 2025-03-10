227f5523cde99e718954fb98c4308fe011e62088
Frankblation <124920027+Frankblation@users.noreply.github.com>
2025-03-10 09:19:09 -0500

    pushing for saftey

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,26 @@
 import { Alert, Pressable, Text, View } from "react-native";
+import { StyleSheet } from "react-native"
 import { styles } from "@/lib/Constants";
 import { Link, router } from "expo-router";
 import { DatabaseProvider } from "@/components/DatabaseProvider";
 import { useActivities } from "@/hooks/useActivities";
+import { FlashList } from "@shopify/flash-list";
+
 export default function Index() {
   const { activities } = useActivities();
 
   return (
       <DatabaseProvider>
           <View style={styles.container}>
+            <View stlye ={styles.list}>
+          <FlashList
+                  renderItem={({ item }) => (
+          <Acti vity steps={item.steps} date={item.date} id={item.id} />
+        )}
+        data={activities}
+        estimatedItemSize={50}
+      />
+      </View>
               <Text style={styles.heading}>{JSON.stringify(activities)}.</Text>
               <Link style={styles.button} href={'/add-activity-screen'}>
                   <Text style={styles.buttonText}>Add Activity</Text>
@@ -17,3 +29,11 @@ export default function Index() {
       </DatabaseProvider>
   );
 }
+
+const styles = StyleSheet.create({
+
+  list: {
+
+
+  }
+)};
